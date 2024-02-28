644a5f55b1f0d99d6ab03d1e7f129200a476c145
unknown <ocirovvadim51@gmail.com>
2024-02-28 19:44:48 +1200

    added getting text and links to images from the last posе

--- README.md
+++
@@ -1,3 +0,0 @@
-# Tg channel - aggregator
-## _The goal of the project is to provide a TG channel that can take posts from VK groups and post them on your channel._
-
--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import os
 from dotenv import load_dotenv
 
 
+
+def take_posts():
+    pass
+       
+
 if __name__ == '__main__':
     load_dotenv()
 
@@ -12,17 +17,27 @@ if __name__ == '__main__':
     token = os.getenv('SERVER_KEY')
     domain = 'kotrastet'
     version = 5.137
+    count = 1
 
     params = {
     'access_token': token,
     'v':version,
     'domain': domain,
-    'count':1
+    'count':count,
     }
     
     response = requests.get(url, params=params)
-    response.raise_for_status()
-    data = response.json()['response']['items']
-    pprint.pprint(data)
+    response.raise_for_status() 
+    text = response.json()['response']['items'][0]['text']
 
-    
+    all_posts = []
+
+    for i in text:
+        all_posts.append(i)
+
+    print(''.join(all_posts))   
+
+    data = response.json()['response']['items'][0]['attachments']
+    for i in data:
+        pprint.pprint(i['photo']['sizes'][-1]['url'])
+        
